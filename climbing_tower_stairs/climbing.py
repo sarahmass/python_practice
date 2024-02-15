8ff56428c5f6bfe988a4538ae24ff55a1230b26d
Sarah Massengill <sarahmassengill@gmail.com>
2024-02-15 12:20:31 -0800

    changes to the graph

--- climbing_tower_stairs/climbing.py
+++ climbing_tower_stairs/climbing.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm 
 import statistics 
 
+
+
 # using random package with in numpy
 np.random.seed(123)
 
@@ -51,22 +53,50 @@ if __name__ == "__main__":
     play_n_times = 10000
     rolls_per_game = 100
     clumsy_rate = 0.001 # clumsy_rate * 100% chances of falling
-    
+    winning_stair = 60 # win if higher than 60 steps after 100 rolls
     
     all_games = []
     final_steps= []
+    fig, ax = plt.subplots(2,1)
+    
+    
     
     for n in range(play_n_times):
         game = play_game(rolls_per_game, clumsy_rate)
+
+        # Sample a few of the random stair falls
+        if n in range(1,play_n_times,500):
+            ax[0].plot(game)
+        
+        # collect final step value
         final_steps.append(game[-1])
+
+        # collect full game details
         all_games.append(game)
-    plt.hist(final_steps)
-    np_final_steps = np.array(final_steps)
-    plt.clf()
-    plt.plot(all_games[0])
-    make_it = np_final_steps >=60
-    print(f"I will make it to step 60, and win the bet {sum(make_it)/len(final_steps)*100:.2f}% of the time")
     
+    np_all_games = np.array(all_games)
+    
+    #plt.clf()
+    # add line at winning_stair to view successful and unsuccessful climbs    
+    ax[0].plot(list(range(101)),[winning_stair]*101, "k-")
+    ax[0].set_title("Sample of Games Played", fontsize=10)
+    ax[0].set_xlabel("roll number",fontsize=8)
+    ax[0].set_ylabel("stair number", fontsize=8)
+        
+
+    # Plot the histogram of all final steps to visualize distribution
+    np_final_steps = np.array(final_steps)
+    
+    ax[1].hist(final_steps)
+    ax[1].set_xlabel("final stair value",fontsize=8)
+    ax[1].set_ylabel("number of games", fontsize=8 )
+    ax[1].set_title(f"Distribution of Final Stair of {play_n_times} Games",fontsize=10  )
+
+    
+    make_it = np_final_steps >=60
+    print(f"{sum(make_it)/len(final_steps)*100:.2f}% of the time the games makes it to/past the {winning_stair}th stair")
+    ax[1].plot([winning_stair]*3200, list(range(0,3200)),'r-' )
+    ax[1].text(62,300,f"     {sum(make_it)/len(final_steps)*100:.2f}% \nmake it to stair {winning_stair}")
     plt.show() 
 
         
